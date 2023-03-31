ee4e1e2add95e8d9c3400e645c6dbac0c6e97e40
José Eduardo Arnhold <106211165+DuduArnhold@users.noreply.github.com>
2023-03-31 10:08:11 -0300

    Adicionando animação no titulo e slider com JS

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ function animaçãoHome(elemento){
 }
 
 animaçãoHome(txtAnimation);
+
+
+const controls = document.querySelectorAll('.control');
+let currentItem = 0;
+const items = document.querySelectorAll('.item');
+const maxItens = items.length;
+
+controls.forEach((control) =>{
+    control.addEventListener('click', (e) =>{
+        const isLeft = e.target.classList.contains("arrow-left");
+            if (isLeft){
+                currentItem -= 1;
+            }else{
+                currentItem += 1;
+            }
+
+            if (currentItem >= maxItens){
+                currentItem = 0;
+            }
+
+            if (currentItem < 0){
+                currentItem = maxItens - 1;
+            }
+        console.log('control clicked', isLeft, currentItem)
+
+
+items.forEach((item) =>{
+    item.classList.remove('current-item');
+})
+
+items[currentItem].scrollIntoView({
+    behavior: "smooth",
+    inline: "center",
+    block: "nearest"
+});
+
+items[currentItem].classList.add("current-item")
+
+})
+})
